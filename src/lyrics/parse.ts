616d6659b2a38b6a405a3584b1300971dc381064
BlingShot_ <loginacceptma@gmail.com>
2026-09-14 19:25:05 +0800

    fixed lyrics parser

--- src/lyrics/parse.ts
+++ src/lyrics/parse.ts
@@ -3,10 +3,10 @@ import { importProjectTtml } from '../studio/projectImport';
 import { projectToPlayer } from '../studio/playerAdapter';
 import { LyricsError } from './types';
 
-export const LYRICS_PARSER_VERSION = 2;
+export const LYRICS_PARSER_VERSION = 3;
 export function parseLyrics(source: string, fileName: string) {
   if (source.length > 2 * 1024 * 1024) throw new LyricsError('Lyrics must be smaller than 2 MB.');
-  if (/\.ttml$/i.test(fileName)) return projectToPlayer(importProjectTtml(source, '', fileName));
+  if (/\.ttml$|\.amll$/i.test(fileName)) return projectToPlayer(importProjectTtml(source, '', fileName));
   if (/\.lrc$/i.test(fileName)) return parseLrc(source);
-  throw new LyricsError('Choose a .ttml or .lrc lyric file.');
+  throw new LyricsError('Choose a .ttml/.amll or .lrc lyric file.');
 }
